9360e28508d4806da5b7af14252026e20f3b2cd8
Francois Wauquier <wokier@gmail.com>
2013-04-09 16:47:36 +0200

    v0.2 GravatarColumn

--- src/com/github/wokier/gwt/gravatar/client/EntryPoint.java
+++ src/com/github/wokier/gwt/gravatar/client/EntryPoint.java
@@ -1,11 +1,33 @@
 package com.github.wokier.gwt.gravatar.client;
 
-import com.github.wokier.gwt.client.Gravatar;
+import com.github.wokier.gwt.client.GravatarColumn;
+import com.github.wokier.gwt.client.GravatarImage;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.view.client.ListDataProvider;
 
 public class EntryPoint implements com.google.gwt.core.client.EntryPoint {
+
     public void onModuleLoad() {
 	RootPanel.get("loading").setVisible(false);
-	RootPanel.get().add(new Gravatar("wokier@gmail.com"));
+	RootPanel.get().add(new GravatarImage("wokier@gmail.com"));
+	CellTable<String> cellTable = new CellTable<String>();
+	cellTable.addColumn(new GravatarColumn<String>() {
+	    @Override
+	    protected String getEmail(String email) {
+		return email;
+	    }
+	}, "Gravatar");
+	cellTable.addColumn(new TextColumn<String>() {
+	    @Override
+	    public String getValue(String email) {
+		return email;
+	    }
+	}, "Email");
+	ListDataProvider<String> dataProvider = new ListDataProvider<String>();
+	dataProvider.addDataDisplay(cellTable);
+	RootPanel.get().add(cellTable);
+	dataProvider.getList().add("wokier@gmail.com");
     }
 }
